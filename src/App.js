d69706cbb892a203758d50c9ecc71d3b31ef5509
james medina <jamesmedina7787@outlook.com>
2019-04-20 14:42:35 -0400

    content added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <p> ` I started seven years ago. I did my first act at an open mic on macdougal
+             street. The first time I performed was completely surreal. As the words
+             traveled out through the audience, I felt as if I was disappearing into
+             the crowd. That is when I became one with my art.<br/><br/>I did a month
+             at first. A few years later, I returned. This time I kept steady at it,
+             once a week, for about six months. It only took a half a year to return
+             this time. Now I do it about three times a week. Even though I've left,
+              I never could stay away. Comedy is hard, but life is harder. Every time
+              life beat me up, it made me realize that the stage is more my place than
+               anywhere else.`</p>
       </div>
     );
   }
