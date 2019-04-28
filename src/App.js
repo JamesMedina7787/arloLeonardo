bdf9562b7961e2f62500bf234b519f3166ec50fa
james medina <jamesmedina7787@outlook.com>
2019-04-28 19:49:12 -0400

    FoldOverMenu added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
 import React, { Component } from 'react';
 import Laugh from "./components/Laugh"
-
+import FoldOverItem from "./components/FoldOverItem"
 import './App.css';
+
+
+var foldOver = {
+  'name':'name',
+  'links':'links',
+  'schedule':'Clips',
+  'blog':'blog'
+}
+
+var Career = {
+  'intro':''
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <Laugh />
+        <div className="foldOverContainer">
+          <div className='foldOver'>
+            <FoldOverItem message={foldOver.name}/>
+          </div>
+          <div className='foldOver'>
+            <FoldOverItem message={foldOver.links}/>
+          </div>
+          <div className='foldOver'>
+            <FoldOverItem message={foldOver.schedule}/>
+          </div>
+          <div className='foldOver'>
+            <FoldOverItem message={foldOver.blog}/>
+          </div>
+         </div>
+         <div classNames="">
+            <Laugh message= {Career.intro} />
+          </div>
       </div>
     );
   }
